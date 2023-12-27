b680fc6e6b9a058faf909d6bfbac91077925a936
mohan <xyz@gmail.com>
2023-12-27 16:16:51 +0530

    secong commit

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import time
 cH = time.strftime('%H:%M:%S')
 print(cH)
 cH = int(time.strftime('%H'))
-a=input("Enter ur name: ")
+a=input("Enter your name: ")
 if cH < 12 :
     print("GM ",a)
 elif 12 <= cH < 18:
