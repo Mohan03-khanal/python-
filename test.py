e52c718afcd65a087c765a2292ee9c95b81c57e7
mohan <xyz@gmail.com>
2023-12-27 15:35:26 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+import time
+cH = time.strftime('%H:%M:%S')
+print(cH)
+cH = int(time.strftime('%H'))
+a=input("Enter ur name: ")
+if cH < 12 :
+    print("GM ",a)
+elif 12 <= cH < 18:
+    print("GA ",a)
+else:
+    print("GE ",a)
